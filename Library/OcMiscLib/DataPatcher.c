a30908679f963b1bc2d9a4d0ea0d5790a8494bb1
jief666 <jief@666>
2024-01-01 13:40:00 +0100

    Redirect patching functions (FindPattern & ApplyPatch) to Clover.

--- Library/OcMiscLib/DataPatcher.c
+++ Library/OcMiscLib/DataPatcher.c
@@ -18,6 +18,62 @@
 #include <Library/DebugLib.h>
 #include <Library/OcMiscLib.h>
 
+#ifdef CLOVER_BUILD
+
+#include "../../../rEFIt_UEFI/Platform/MemoryOperation.h"
+
+
+BOOLEAN
+FindPattern (
+  IN CONST UINT8   *Pattern,
+  IN CONST UINT8   *PatternMask OPTIONAL,
+  IN CONST UINT32  PatternSize,
+  IN CONST UINT8   *Data,
+  IN UINT32        DataSize,
+  IN UINT32        *DataOff
+  )
+{
+
+//  ASSERT (DataOff >= 0);
+
+  if (PatternSize == 0 || DataSize == 0 || *DataOff >= DataSize || DataSize - *DataOff < PatternSize) {
+    return FALSE;
+  }
+
+  UINTN result = FindMemMask(Data + *DataOff, DataSize, Pattern, PatternSize, PatternMask, PatternSize);
+  if (result != MAX_UINTN) {
+    if ( result < MAX_INT32 - *DataOff ) {
+      *DataOff = (UINT32)result;
+      return TRUE;
+    }
+  }
+  return FALSE;
+}
+
+UINT32
+ApplyPatch (
+  IN CONST UINT8   *Pattern,
+  IN CONST UINT8   *PatternMask OPTIONAL,
+  IN CONST UINT32  PatternSize,
+  IN CONST UINT8   *Replace,
+  IN CONST UINT8   *ReplaceMask OPTIONAL,
+  IN UINT8         *Data,
+  IN UINT32        DataSize,
+  IN UINT32        Count,
+  IN UINT32        Skip
+  )
+{
+  UINTN res = SearchAndReplaceMask(Data, DataSize, Pattern, PatternMask, PatternSize, Replace, ReplaceMask, Count, Skip);
+  if ( res < MAX_UINT32 ) {
+    return (UINT32)res;
+  }else{
+    return MAX_UINT32;
+  }
+}
+
+
+#else
+
 STATIC
 BOOLEAN
 InternalFindPattern (
@@ -158,13 +214,63 @@ ApplyPatch (
     //
     // Perform replacement.
     //
+    #ifdef JIEF_DEBUG
+      UINTN length =
+          (
+               AsciiStrLen("Replace ") +
+               PatternSize*2 +
+               (PatternMask ? 1+PatternSize*2+1+PatternSize*2+1 : 0) +
+               AsciiStrLen(" by ") +
+               PatternSize*2 +
+               (ReplaceMask ? 1+PatternSize*2+1+PatternSize*2+1 : 0) +
+               AsciiStrLen(" at ofs:%X\n") + 8 // +16 for %X
+          ) * 1 + 1; // *2 for CHAR16, +1 for \0
+      CHAR8* buf = AllocateZeroPool(length);
+      AsciiSPrint(buf, length, "%aReplace ", buf );
+      for (UINTN Index = 0; Index < PatternSize; ++Index) {
+        AsciiSPrint(buf, length, "%a%02X", buf, Pattern[Index]);
+      }
+      if ( PatternMask ) {
+        AsciiSPrint(buf, length, "%a/", buf );
+        for (UINTN Index = 0; Index < PatternSize; ++Index) {
+          AsciiSPrint(buf, length, "%a%02X", buf, PatternMask[Index]);
+        }
+        AsciiSPrint(buf, length, "%a(", buf );
+        for (UINTN Index = 0; Index < PatternSize; ++Index) {
+          AsciiSPrint(buf, length, "%a%02X", buf, Data[(UINT32)DataOff + Index]); // Safe cast, DataOff is >= 0
+        }
+        AsciiSPrint(buf, length, "%a)", buf );
+      }
+      AsciiSPrint(buf, length, "%a by ", buf );
+    #endif
     if (ReplaceMask == NULL) {
-      CopyMem (&Data[DataOff], Replace, PatternSize);
+      CopyMem (&Data[DataOff], (void*)Replace, PatternSize);
     } else {
       for (UINTN Index = 0; Index < PatternSize; ++Index) {
         Data[DataOff + Index] = (Data[DataOff + Index] & ~ReplaceMask[Index]) | (Replace[Index] & ReplaceMask[Index]);
       }
     }
+
+    #ifdef JIEF_DEBUG
+      for (UINTN Index = 0; Index < PatternSize; ++Index) {
+        AsciiSPrint(buf, length, "%a%02X", buf, Replace[Index]);
+      }
+      if ( ReplaceMask ) {
+        AsciiSPrint(buf, length, "%a/", buf );
+        for (UINTN Index = 0; Index < PatternSize; ++Index) {
+          AsciiSPrint(buf, length, "%a%02X", buf, ReplaceMask[Index]);
+        }
+        AsciiSPrint(buf, length, "%a(", buf );
+        for (UINTN Index = 0; Index < PatternSize; ++Index) {
+          AsciiSPrint(buf, length, "%a%02X", buf, Data[(UINT32)DataOff + Index]); // Safe cast, DataOff is >= 0
+        }
+        AsciiSPrint(buf, length, "%a)", buf );
+      }
+
+      AsciiSPrint(buf, length, "%a at ofs:%X\n", buf, DataOff);
+      DEBUG((DEBUG_VERBOSE, "%a", buf ));
+      FreePool(buf);
+    #endif
 
     ++ReplaceCount;
     DataOff += PatternSize;
@@ -182,3 +288,5 @@ ApplyPatch (
 
   return ReplaceCount;
 }
+
+#endif
