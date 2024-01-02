59717698e064878e6c5aab206b33e1b2d5be3434
jief666 <jief@666>
2024-01-02 15:27:45 +0100

    Add missing arg in OcDebugLogLibOc2Clover/OcConfigureLogProtocol (TODO:group)

--- Library/OcDebugLogLibOc2Clover/OcLog.c
+++ Library/OcDebugLogLibOc2Clover/OcLog.c
@@ -38,6 +38,7 @@
 EFI_STATUS
 OcConfigureLogProtocol (
   IN OC_LOG_OPTIONS                   Options,
+  IN CONST CHAR8                      *LogModules,
   IN UINT32                           DisplayDelay,
   IN UINTN                            DisplayLevel,
   IN UINTN                            HaltLevel,
@@ -46,6 +47,7 @@ OcConfigureLogProtocol (
   )
 {
   (void)Options;
+  (void)LogModules;
   (void)DisplayDelay;
   (void)DisplayLevel;
   (void)HaltLevel;
